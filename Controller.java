69495c6ae587ac998150ec52a40663069ae2076d
aubreygonzalez17 <63797416+aubreygonzalez17@users.noreply.github.com>
2020-04-21 15:37:14 -0500

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -9,24 +9,36 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.event.*;
-
 import java.awt.event.MouseEvent;
 import java.beans.EventHandler;
 
 
 public class Controller {
+    /**
+     * Defines the methods responsible for controlling the main user interface
+     */
+
     View view = new View();
 
-
     public void handleButtonAction(ActionEvent event, ComboBox comboBox)
+    /**
+     * Called when the user wants to launch the exercise they selected.
+     *
+     * @param event identifies the button click event
+     * @param comboBox identifies the ComboBox item corresponding to the exercise to launch
+     */
     {
         Stage primaryStage = new Stage();
-        System.out.println("You clicked me!");
         String selected = (String)comboBox.getValue();
-        if (selected == "Exercise 1")
-        {
+
+        if (selected == "Exercise 1") {
+            // Launch new duplicate window when "Exercise 1" is selected
             view.newGUI(primaryStage);
         }
-    }
 
+        else {
+            //Leaving blank to add future exercises
+            System.out.println("Exercise not available. Please select another");
+        }
+    }
 }
